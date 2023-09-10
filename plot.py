373c7a4216e95c49cb8aad9c9f2000551a84f9e3
AlessandroCanossa <alessandro.canossa.99@gmail.com>
2023-09-10 23:55:53 +0000

    add fast feature extraction + plot vs cv2
    Signed-off-by: AlessandroCanossa <alessandro.canossa.99@gmail.com>

---
+++ plot.py
@@ -0,0 +1,38 @@
+import cv2
+import pathlib
+
+
+image = cv2.imread("images/lena.png", cv2.IMREAD_GRAYSCALE)
+feature_file = pathlib.Path("features.csv")
+
+features = []
+
+lines = []
+with open(feature_file, "r") as f:
+    lines = f.readlines()
+
+for line in lines:
+    x, y, score = line.split(',')
+    x = float(x)
+    y = float(y)
+    score = float(score)
+
+    features.append(cv2.KeyPoint(x, y, score))
+
+new_image = cv2.drawKeypoints(image, features, None, color=(255, 0, 0))
+
+fast = cv2.FastFeatureDetector_create()
+print(f"Threshold: {fast.getThreshold()}")
+print("nonmaxSuppression:{}".format(fast.getNonmaxSuppression()))
+print("neighborhood: {}".format(fast.getType()))
+
+fast.setThreshold(50)
+
+kpts = fast.detect(image, None)
+
+new_image2 = cv2.drawKeypoints(image, kpts, None, color=(255, 0, 0))
+
+cv2.imshow("image1", image)
+cv2.imshow("image2", new_image)
+cv2.imshow("image3", new_image2)
+cv2.waitKey(0)
