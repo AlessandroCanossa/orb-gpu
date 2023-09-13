2ffe54e022e972c33caa2ff18fced248dd66f736
AlessandroCanossa <alessandro.canossa.99@gmail.com>
2023-09-13 23:13:21 +0000

    finished orb

--- src/accum.cu
+++ src/accum.cu
@@ -3,15 +3,30 @@
 #include <thrust/device_ptr.h>
 #include <thrust/device_vector.h>
 
-struct if_even
+struct if_corner
 {
-    __host__ __device__ bool operator()(Kp x) { return x.isCorner; }
+    __host__ __device__ bool operator()(Corner x) { return x.isCorner; }
 };
 
-void collect(Kp* kpts, int size, Kp* out, cudaStream_t aStream)
+void collect(Corner* kpts, int size, Corner* out, cudaStream_t aStream)
 {
-    thrust::device_ptr<Kp> prova = thrust::device_pointer_cast(kpts);
-    thrust::device_ptr<Kp> output = thrust::device_pointer_cast(out);
+    thrust::device_ptr<Corner> prova = thrust::device_pointer_cast(kpts);
+    thrust::device_ptr<Corner> output = thrust::device_pointer_cast(out);
 
-    thrust::copy_if(thrust::cuda::par.on(aStream), prova, prova + size, output, if_even());
+    thrust::copy_if(thrust::cuda::par.on(aStream), prova, prova + size, output, if_corner());
+}
+
+__global__ void toKeypoints(Corner* aCorners, int aSize, Keypoint* aKeypoints)
+{
+    int id = blockDim.x * blockIdx.x + threadIdx.x;
+    if (id > aSize) {
+        return;
+    }
+
+    aKeypoints[id].corner = aCorners[id];
+}
+
+void toKpts(Corner* aCorners, int aSize, Keypoint* aKeypoints, cudaStream_t aStream)
+{
+    toKeypoints<<<ceil((double)aSize / 32), 32, 0, aStream>>>(aCorners, aSize, aKeypoints);
 }
--- src/gaussian_blur.cu
+++ src/gaussian_blur.cu
@@ -1,24 +1,37 @@
 #include "orb_gpu/cuda.cuh"
 
+__device__ inline uint8_t getPixelValue(Image& aImg, int x, int y)
+{
+    return (x < 0 || x >= aImg.width || y < 0 || y >= aImg.height) ? 125 : aImg.getValue(x, y);
+}
+
 __global__ void gaussianBlur(Image aInput, Image aOutput)
 {
-
     size_t idx = blockIdx.x * blockDim.x + threadIdx.x;
     size_t idy = blockIdx.y * blockDim.y + threadIdx.y;
 
-    __shared__ uint32_t kernel[5*5];
-
-    if (idx < 3 || idx > aInput.width - 3 || idy < 3 || idy > aInput.height - 3) {
+    if (idx > aInput.width || idy > aInput.height) {
         return;
     }
+
+    uint32_t result = 0;
+#pragma unroll
+    for (int i = -2; i <= 2; ++i) {
+#pragma unroll
+        for (int j = -2; j <= 2; ++j) {
+            int kernelVal = KERNEL_GAUSS_5[i + 2 + (j + 2) * 5];
+            int pixelVal = getPixelValue(aInput, idx + i, idy + j);
+            result += (pixelVal * kernelVal);
+        }
+    }
+    result /= GAUSS_M;
+    aOutput.setValue(idx, idy, result);
 }
 
-cudaError_t callGaussianBlur(const Image& aImg, Image& aOutputImage, cudaStream_t aStream)
+void callGaussianBlur(const Image& aImg, Image& aOutputImage, cudaStream_t aStream)
 {
-    dim3 gridDim{ static_cast<unsigned int>((aImg.width - 1) / 32 + 1),
-                  static_cast<unsigned int>((aImg.height - 1) / 32 + 1) };
-    dim3 blockDim{ 32, 32 };
+    dim3 gridDim(ceil((double)aImg.width / 32), ceil((double)aImg.height / 8));
+    dim3 blockDim(32, 8);
 
     gaussianBlur<<<gridDim, blockDim, 0, aStream>>>(aImg, aOutputImage);
-    return cudaStreamSynchronize(aStream);
 }
